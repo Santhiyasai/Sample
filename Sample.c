cd2dbc3151dcd4b851a1da665b5b123e109ea83d
Santhiyasai <41000848+Santhiyasai@users.noreply.github.com>
2019-03-27 12:57:47 +0530

    Update and rename Sample.travis.yml to Sample.c

---
+++ Sample.c
@@ -0,0 +1,25 @@
+//#define signal
+#include <stdio.h>
+int main()
+{
+int signal[50]={1,1,0,0,1,0,0,1,0,1,0,0,1,1,1,0,0,1,0,1,0,0,0,1,1,0,1,0,0,1,1,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,0,1,1};
+int i,j;
+for(i=1,j=i+1;i<=50,j<=51;i++,j++)
+{
+//j=i+1;
+if(signal[i]==signal[j])
+{
+printf("Level");
+}
+else if(signal[i]<signal[j])
+{
+printf("Rising Edge Detected");
+}
+else if(signal[i]>signal[j])
+{
+printf("Falling Edge Detected");
+}
+}
+return 0;
+}
+
